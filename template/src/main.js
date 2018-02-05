3eb1496f61b67b1f3773f9343c41a2012e5f1723
Leon Hyoung <574857444@qq.com>
2018-02-06 00:33:46 +0800

    Initial commit

---
+++ template/src/main.js
@@ -0,0 +1,38 @@
+{{#if_eq build "standalone"}}
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+{{/if_eq}}
+import Vue from 'vue'
+import App from './App'
+{{#router}}
+import router from './router'
+{{/router}}
+{{#vuex}}
+import store from './store'
+{{/vuex}}
+{{#i18n}}
+import i18n from './i18n'
+{{/i18n}}
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  {{#router}}
+  router,
+  {{/router}}
+{{#i18n}}
+  i18n,
+  {{/i18n}}
+  {{#vuex}}
+  store,
+  {{/vuex}}
+  {{#if_eq build "runtime"}}
+  render: h => h(App)
+  {{/if_eq}}
+  {{#if_eq build "standalone"}}
+  components: { App },
+  template: '<App/>'
+  {{/if_eq}}
+})
